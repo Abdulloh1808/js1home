0b1843e592e71eb4f308af1dfeb491ab30d7abeb
abdulloh <abdulloh@gmail.com>
2021-12-25 14:38:30 +0500

    js1home

---
+++ script.js
@@ -0,0 +1,42 @@
+var name = prompt ('Ismingizni kiriting:') 
+console.log('Sizning ismingiz: ' + name );
+var age = +prompt('Yoshingizni kiriting:' )
+console.log('Sizning yoshingiz: '+age ); 
+var answer = +prompt('Misolni yeching: 10+5= ? ')
+    if (10 + 5 == answer) {
+        console.log("sizning javobingiz to'g'ri: " + answer);
+    } else if (10 + 5 != answer) {
+        console.log("Sizning javobingiz noto'g'ri:"+answer+ "  to'g'ri javob: " + (10 + 5));
+    }
+var answer = +prompt('Misolni yeching: 10-5= ? ')
+    if (10 - 5 == answer) {
+        console.log("sizning javobingiz to'g'ri: " + answer);
+    } else if (10 - 5 != answer) {
+        console.log("Sizning javobingiz noto'g'ri:"+answer+ "  to'g'ri javob: " + (10 - 5));
+    }
+var answer = +prompt('Misolni yeching: 10*5= ? ')
+    if (10 * 5 == answer) {
+        console.log("sizning javobingiz to'g'ri: " + answer);
+    } else if (10 * 5 != answer) {
+        console.log("Sizning javobingiz noto'g'ri:"+answer+ "  to'g'ri javob: " + 10 * 5);
+    }
+var answer = +prompt('Misolni yeching: 10/5= ? ')
+    if (10 / 5 == answer) {
+        console.log("sizning javobingiz to'g'ri: " + answer);
+    } else if (10 / 5 != answer) {
+        console.log("Sizning javobingiz noto'g'ri:"+answer+ "  to'g'ri javob: " + 10 / 5);
+    }
+var answer = +prompt("misolning qoldig'ini toping: 10 % 3= ?")
+    if(answer == 10 % 3 ==1){
+        console.log("sizning javobingiz to'g'ri: " + answer);
+    }
+    else if (answer != 10 % 3 == 1){
+        console.log("Sizning javobingiz noto'g'ri:"+answer + "  To'g'ri javob: " + 10 % 3);
+    }
+// var answer = +prompt("misolning qoldig'ini toping: 8 % 10= ?")
+//     if(answer == 8 % 10 ==0){
+//         console.log("sizning javobingiz to'g'ri: " + answer);
+//     }
+//     else if (answer != 8 % 10 == 0){
+//         console.log("Sizning javobingiz noto'g'ri.  " + "to'g'ri javob: " + 8 % 10);
+//     }
